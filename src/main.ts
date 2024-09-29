bdad6312df90cb0ba03745daa53fd1b3aaab8dec
Vishnu11Venkat <vishnu4venkat@gmail.com>
2024-09-29 23:56:34 +0530

    vercel

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Server } from 'http';
 
-async function bootstrap() {
+async function bootstrap(): Promise<Server> {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3001);
+  await app.listen(3001); // You can set your own port if needed
+  return app.getHttpAdapter().getInstance();
 }
-bootstrap();
+
+export default bootstrap();
+
